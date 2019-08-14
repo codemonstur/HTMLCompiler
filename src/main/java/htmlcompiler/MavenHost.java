d44cdf5988ebc8f2b447c4ca39be98407c65ace9
JurgenNED <jegvoorneveld@gmail.com>
2019-08-14 22:28:09 +0200

    Commit
    Removed Undertow

--- src/main/java/htmlcompiler/MavenHost.java
+++ src/main/java/htmlcompiler/MavenHost.java
@@ -1,11 +1,8 @@
 package htmlcompiler;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import htmlcompiler.tools.LogSuppressingMojo;
-import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.handlers.BlockingHandler;
-import io.undertow.util.Headers;
-import io.undertow.util.StatusCodes;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -13,17 +10,13 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
+import java.net.InetSocketAddress;
 import java.nio.file.Files;
 
 import static htmlcompiler.Tasks.*;
 import static htmlcompiler.tools.Logger.newLogger;
 import static htmlcompiler.tools.Watcher.watchDirectory;
-import static io.undertow.UndertowOptions.ALWAYS_SET_KEEP_ALIVE;
-import static io.undertow.UndertowOptions.ENABLE_HTTP2;
 
 @Mojo( name = "host" )
 public final class MavenHost extends LogSuppressingMojo  {
@@ -38,16 +31,13 @@ public final class MavenHost extends LogSuppressingMojo  {
     public void execute() throws MojoFailureException {
         final Log log = getLog();
 
-        Undertow.builder()
-            .setServerOption(ALWAYS_SET_KEEP_ALIVE, true)
-            .setServerOption(ENABLE_HTTP2, true)
-            .addHttpListener(port, "127.0.0.1")
-            .setHandler(pathHandler(toStaticDirectory(project), toOutputDirectory(project)))
-            .build().start();
-
-        log.info("Listening on localhost:"+port);
-
         try {
+            final HttpServer server = HttpServer.create(new InetSocketAddress("127.0.0.1", port), 100);
+            server.createContext("/", pathHandler(toStaticDirectory(project), toOutputDirectory(project)));
+            server.start();
+
+            log.info("Listening on localhost:"+port);
+
             watchDirectory(toInputDirectory(project), () -> compileHTML(newLogger(log::info, log::warn), project));
         } catch (IOException e) {
             throw new MojoFailureException(e.getMessage());
@@ -55,19 +45,21 @@ public final class MavenHost extends LogSuppressingMojo  {
     }
 
     private static HttpHandler pathHandler(final File... directories) {
-        return new BlockingHandler(exchange -> {
+        return exchange -> {
             for (final File dir : directories) {
-                final File file = toFile(dir, exchange.getRequestPath(), null);
+                final File file = toFile(dir, exchange.getRequestURI().getPath(), null);
                 if (file == null) continue;
 
-                exchange.setStatusCode(StatusCodes.OK);
-                exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, Files.probeContentType(file.toPath()));
-                exchange.getResponseSender().transferFrom(toFileChannel(file), null);
+                exchange.getResponseHeaders().add("Content-Type", Files.probeContentType(file.toPath()));
+                exchange.sendResponseHeaders(200, file.length());
+                Files.copy(file.toPath(), exchange.getResponseBody());
+                exchange.close();
                 return;
             }
 
-            exchange.setStatusCode(StatusCodes.NOT_FOUND);
-        });
+            exchange.sendResponseHeaders(404, 0);
+            exchange.close();
+        };
     }
 
     private static File toFile(final File dir, final String requestPath, final File _default) {
@@ -77,9 +69,5 @@ public final class MavenHost extends LogSuppressingMojo  {
 
     private static boolean isChildOf(final File child, final File parent) {
         return child.toPath().startsWith(parent.toPath());
-    }
-
-    private static FileChannel toFileChannel(final File file) throws FileNotFoundException {
-        return new RandomAccessFile(file, "r").getChannel();
     }
 }
