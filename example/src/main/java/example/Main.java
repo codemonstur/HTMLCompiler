d457bf7dce3e6a5955fe2809ce40656fd4fc6006
JurgenNED <jegvoorneveld@gmail.com>
2019-10-07 22:31:29 +0200

    Commit
    Removed Undertow as a dependency of the example project

--- example/src/main/java/example/Main.java
+++ example/src/main/java/example/Main.java
@@ -1,22 +1,27 @@
 package example;
 
-import io.undertow.Undertow;
+import com.sun.net.httpserver.HttpServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public enum Main {;
 
     public static void main(final String... args) throws IOException {
-        final String page = resourceToString("/webbin/index.html");
+        final byte[] page = resourceToString("/webbin/index.html").getBytes(UTF_8);
 
-        Undertow.builder()
-            .addHttpListener(8080, "127.0.0.1")
-            .setHandler(e -> e.getResponseSender().send(page))
-            .build().start();
+        HttpServer
+            .create(new InetSocketAddress("127.0.0.1", 8080), 100)
+            .createContext("/", exchange -> {
+                exchange.sendResponseHeaders(200, page.length);
+                exchange.getResponseBody().write(page);
+                exchange.close();
+            })
+            .getServer().start();
     }
 
     private static String resourceToString(final String resource) throws IOException {
