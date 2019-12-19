0a60208fa8bb963b63abe60eb0b319951dc99e29
Jurgen Voorneveld <jurgen@ngti.nl>
2019-12-19 17:08:25 +0100

    Fixed bug in style parsing, added style tag merging

--- src/main/java/htmlcompiler/compile/tags/Style.java
+++ src/main/java/htmlcompiler/compile/tags/Style.java
@@ -1,6 +1,7 @@
 package htmlcompiler.compile.tags;
 
 import htmlcompiler.tools.IO;
+import org.w3c.dom.Node;
 
 import java.io.File;
 
@@ -19,15 +20,33 @@ public enum Style {;
                 element.removeAttribute("inline");
                 element.removeAttribute("src");
                 element.setTextContent(compressCssCode(IO.toString(location)));
-                return true;
+
+                final Node previousSibling = element.getPreviousSibling();
+                if (isStyle(previousSibling) && !isEmpty(previousSibling)) {
+                    element.setTextContent(previousSibling.getTextContent() + element.getTextContent());
+                    element.getParentNode().removeChild(previousSibling);
+                }
+
+                return false;
             }
             if (!isEmpty(element)) {
                 element.setTextContent(compressCssCode(element.getTextContent()));
+
+                final Node previousSibling = element.getPreviousSibling();
+                if (isStyle(previousSibling) && !isEmpty(previousSibling)) {
+                    element.setTextContent(previousSibling.getTextContent() + element.getTextContent());
+                    element.getParentNode().removeChild(previousSibling);
+                }
+
             }
             if (element.hasAttribute("to-absolute")) {
                 makeAbsolutePath(element, "src");
             }
             return false;
         };
+    }
+
+    private static boolean isStyle(final Node node) {
+        return node != null && "style".equals(node.getNodeName());
     }
 }
