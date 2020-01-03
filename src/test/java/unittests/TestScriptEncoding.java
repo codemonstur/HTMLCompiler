4351a21970b719014689a0f60ec612089be2c065
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2020-01-03 18:57:53 +0100

    Fixed a unit test, updated version of neko-htmlunit, made htmlunit turn tags into lowercase

--- src/test/java/unittests/TestScriptEncoding.java
+++ src/test/java/unittests/TestScriptEncoding.java
@@ -12,14 +12,14 @@ import static junit.framework.Assert.assertEquals;
 
 public class TestScriptEncoding {
 
-    private static final String html_output_uppercase =
-        "<!DOCTYPE html><html><head><title>Test html</title><script>if (1 > 2) console.log(\"Hello, world!\");if (2 > 1) console.log(\"Hello, world!\");</script></head><body></body></html>";
+    private static final String html_output_script_encode =
+        "<!DOCTYPE html><html><head><title>Test html</title><script>if(1>2){console.log(\"Hello, world!\")}if(1<2){console.log(\"Hello, world!\")};</script></head><body></body></html>";
 
     @ParameterizedTest
     @MethodSource("util.Factory#provideCompilers")
-    public void compileUppercase(final HtmlCompiler compiler) throws IOException, InvalidInput {
+    public void compileScriptEncode(final HtmlCompiler compiler) throws IOException, InvalidInput {
         final String output = Parsing.compileFile(compiler, "src/test/resources/html/scripts.html");
-        assertEquals("Invalid generated HTML", html_output_uppercase, output);
+        assertEquals("Invalid generated HTML", html_output_script_encode, output);
     }
 
 }
