d2fd38b4dd50e8beef9a262f846db2d6eb002942
Jurgen <jegvoorneveld@gmail.com>
2020-08-18 19:27:14 +0200

    Fixed bug with 604 file loading

--- src/main/java/htmlcompiler/pojos/httpmock/Request.java
+++ src/main/java/htmlcompiler/pojos/httpmock/Request.java
@@ -7,8 +7,7 @@ import htmlcompiler.services.HttpHandlers;
 import java.nio.file.Path;
 import java.util.List;
 
-import static htmlcompiler.services.HttpHandlers.sendFile;
-import static htmlcompiler.services.HttpHandlers.toFile;
+import static htmlcompiler.services.HttpHandlers.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class Request {
@@ -29,13 +28,15 @@ public final class Request {
         if (request.statusCode == 604) { // 604 is 2 times a 302, we want to send the file at Location
             final var location = toLocationHeader(request);
             if (location == null) return HttpHandlers::send404;
-
-            for (final Path dir : directories) {
-                final var file = toFile(dir, location.value, null);
-                if (file == null) continue;
-                return exchange -> sendFile(exchange, file);
-            }
-            return HttpHandlers::send404;
+            return exchange -> {
+                for (final Path dir : directories) {
+                    final var file = toFile(dir, location.value, null);
+                    if (file == null) continue;
+                    sendFile(exchange, file);
+                    return;
+                }
+                send404(exchange);
+            };
         } else return exchange -> {
             final Headers responseHeaders = exchange.getResponseHeaders();
             for (final Header header : request.headers) {
